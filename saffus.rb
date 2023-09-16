226b078e3e9f386330074aea8d0ffaab9505f40f
Yousaf Nabi <yousafn@gmail.com>
2023-09-16 01:50:18 +0100

    Create saffus.rb

---
+++ saffus.rb
@@ -0,0 +1,33 @@
+# typed: false
+# frozen_string_literal: true
+
+class Saffus < Formula
+  desc "CLI for running Cirrus Tasks locally in Docker containers and Virtual Machines"
+  homepage "https://github.com/you54f/cirrus-cli"
+  version "0.98.0"
+
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/you54f/cirrus-cli/releases/download/v0.98.0/saffus-darwin-amd64.tar.gz"
+      sha256 "eaad343a9a14598217b821a8c1eb9cbbdeb82bf071a482b9ef5f7046de306d04"
+
+      def install
+        bin.install "saffus"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/you54f/cirrus-cli/releases/download/v0.98.0/saffus-darwin-arm64.tar.gz"
+      sha256 "6ecfa6198d197c7f47268826e5fc2e3f6d54e82e2e4f0c926125397fb5fb9cd6"
+
+      def install
+        bin.install "saffus"
+      end
+    end
+  end
+
+  def caveats
+    <<~EOS
+      See the Github repository for more information
+    EOS
+  end
+end
